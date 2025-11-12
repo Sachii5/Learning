dfb30d7360897fc1ea11e7f62d1280ea0b818c1f
sachii5 <adillaadilla83@gmail.com>
2025-11-12 20:39:35 +0700

    nambah golang

---
+++ golang/helloworld.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+	fmt.Println("Hello world")
+	nama := "Alber"
+	// nama = "galih"
+	fmt.Println(nama)
+}
---
+++ golang/myname.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main()  {
+	nama := "Alber"
+	var a uint8 = nama[1]
+	fmt.Println(string(a))
+}
