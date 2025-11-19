d889d4983bd4af3e207a94262b035f6d4afa9d6d
alber <adillaadilla83@gmail.com>
2025-11-19 17:48:55 +0700

    function

--- golang/function.go
+++ golang/function.go
@@ -2,18 +2,48 @@ package main
 
 import "fmt"
 
+// ======== FUNCTION AS A VALUE ========== //
+
 func sayName(name string)string {
 	name = "Hello " + name
 	return name
 }
 
+// ======== MULTIPLE RETURN VALUES ========== //
+
 func hewan(hewan1 string, hewan2 string) (string, string){
 	return hewan1, hewan2
 }
 
-func main() {
-	name := sayName("Alber")
-	fmt.Println(name)
+// ========= VARARGS / VARIABLE ARGUMENTS ========== //
 
-	fmt.Println(hewan("Kucing", "Anjing"))
+func sumAll(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+// ================= FUNCTION AS PARAMETER ================ //
+func checkNumber(number int, check func(int) string) string {
+	fmt.Println("Satuan dari", number, "adalah" , check(number))
+}
+
+func satuan(number int) string {
+	if len()
+}
+
+// ================ MAIN FUNCTION ================ //
+
+func main() {
+	name := sayName // Function as a value
+	fmt.Println(name("Alber"))
+
+	fmt.Println(hewan("Kucing", "Anjing")) // Multiple return values
+
+
+	fmt.Println(sumAll(1, 2, 3, 4, 5)) // Mengirim beberapa argumen ke varargs
+	numbers := []int{10, 20, 30, 40, 50}
+	fmt.Println(sumAll(numbers...)) // Mengirim slice ke varargs
 }
